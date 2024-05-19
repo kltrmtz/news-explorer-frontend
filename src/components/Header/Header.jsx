19ce6e94ae6560b44b03ef1a5f4c913f220e2db6
Kolt Metz <kltrmtz@gmail.com>
2024-05-18 20:42:40 -0400

    Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,58 @@
+import "./Header.css";
+// import { Link } from "react-router-dom/cjs/react-router-dom";
+import logout from "/src/images/logout.svg";
+
+const Header = ({ onCreateModal, isLoggedIn }) => {
+  console.log("Header");
+
+  return (
+    <header className="header">
+      <div className="header__title">
+        NewsExplorer
+        <div>
+          {/* <Link to="/">
+            <div className="header__link">Home</div>
+          </Link> */}
+        </div>
+      </div>
+      <div className="header__interactive">
+        {isLoggedIn && (
+          <div>
+            <button
+              className="header__article-button"
+              type="text"
+              onClick={onCreateModal}
+            >
+              Saved articles
+            </button>
+          </div>
+        )}
+        {!isLoggedIn && (
+          <div>
+            <button
+              className="header__signin-button"
+              type="text"
+              onClick={onCreateModal}
+            >
+              Sign In
+            </button>
+          </div>
+        )}
+        {isLoggedIn && (
+          <div>
+            <button
+              className="header__user-logout-button"
+              type="text"
+              onClick={onCloseModal}
+            >
+              {name}
+              <img className="header__logout" src={logout} alt="logout" />
+            </button>
+          </div>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Header;

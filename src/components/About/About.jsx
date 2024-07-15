651e5a612bd3438110b42dcb640fb4dcf33a281d
Kolt Metz <kltrmtz@gmail.com>
2024-07-15 15:19:24 -0400

    About Update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,8 +8,15 @@ function About() {
       <div className="about__content">
         <h2 className="about__title">About the author</h2>
         <p className="about__description">
-          His name is John Cena!! You can't see me... Im a software guy & I like
-          turtles.
+          This project was completed by Kolt Metz, an aspiring software
+          engineer. Versed in full-stack skills HTML, CSS, Javascript, React,
+          Express, Node.js, Figma, MongoDB. Able to produce source code for
+          websites, applications, & software to provide solutions for the end
+          users.
+          <br></br>
+          <br></br>The skills acquired to develop this project was learned
+          through TripleTen a professional bootcamp. A wonderful experience
+          providing the knowledge & oppurtunity for an entry level career.
         </p>
       </div>
     </section>
